ee2a060bb11337a1fd25211918d6f1d9ffbefc1f
NihBertolo <nickbertolo@gmail.com>
2021-02-08 06:51:18 -0300

    Deploy 2

--- src/main/java/com/nicholasbertolo/app/resources/ProdutoResource.java
+++ src/main/java/com/nicholasbertolo/app/resources/ProdutoResource.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.nicholasbertolo.app.entities.Categoria;
 import com.nicholasbertolo.app.entities.Produto;
 import com.nicholasbertolo.app.services.ProdutoService;
 
